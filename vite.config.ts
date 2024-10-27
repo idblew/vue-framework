ae293c303276246c1bdb89ee6f8246fd67d6ee39
Ian Blew <idblew@gmail.com>
2024-10-27 11:10:08 +0000

    Fix JS API deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             sass: {
-                api: "modern-compiler",
+                api: "modern",
             }
         },
     },
